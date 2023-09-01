6df3fcb0078f096a996ddc4d7fd3f58299df71a8
yusukehirao <yusukehirao@me.com>
2023-09-01 10:10:26 +0900

    Setup SvelteKit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<svelte:head>
+	<title>アクセシビリティカンファレンス福岡</title>
+</svelte:head>
+
+<main>
+	<section>
+		<h1>アクセシビリティカンファレンス福岡</h1>
+		<p>
+			<span>2023年11月11日（土曜日）</span>
+			<span>LINE Fukuoka</span>
+		</p>
+		<p>詳細 近日公開</p>
+	</section>
+	<h2>スポンサー募集</h2>
+	<p>
+		スポンサーの募集を開始しました。
+		<a href="https://docs.google.com/presentation/d/1nf6NAYliuFI-MJVTABR0aj1WJMTYt7nwdmcpTUBrpEw">
+			アクセシビリティカンファレンス福岡 企画概要 (Googleスライド)</a
+		>をご覧ください。
+	</p>
+	<p>
+		申し込みは<a
+			href="https://docs.google.com/forms/d/e/1FAIpQLScx1br1WcjicvAX61q_bTuRjyzOldJmfqHdUjtKbCvQs12dRA/viewform"
+			>Googleフォーム</a
+		>からお申し込みください。<strong>先着順</strong>となりますのでお早めにお申し込みください。
+	</p>
+	<p>※資料に情報保障が必要な場合はお問い合わせください</p>
+
+	<h2>お問い合わせ</h2>
+	<p>
+		<a href="mailto:fukuokaa11yconf@googlegroups.com">fukuokaa11yconf@googlegroups.com</a>
+	</p>
+</main>
