f6e2e8d85b1eb57fa8fd424fc57c7bf91cf5460d
YusukeHirao <hirao@d-zero.co.jp>
2024-09-05 23:17:29 +0900

    FONTPLUSのページ遷移時のリロードを実装

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script>
 	import Footer from '$lib/components/footer.svelte';
 	import '../global.css';
+
+	import { page } from '$app/stores';
+	import { browser } from '$app/environment';
+
+	function reload() {
+		if (browser) {
+			// @ts-ignore
+			window.FONTPLUS.reload(true);
+		}
+	}
+
+	$: {
+		console.log('Transition to:', $page.url.pathname);
+		reload();
+	}
 </script>
 
 <slot />
